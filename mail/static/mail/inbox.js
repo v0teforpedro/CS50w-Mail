b5843beac40f9997ebfa85551891845c19cafd54
Peter Sushkin <sushkin.p@gmail.com>
2022-12-06 21:33:40 +0200

    Changed look of navbar, and partially implemented "Mailbox", also upgraded Bootstrap

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,42 +1,72 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+    // Use buttons to toggle between views
+    document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+    document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+    document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+    document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
-  load_mailbox('inbox');
+    // By default, load the inbox
+    load_mailbox('inbox');
 
-  // Send mail on form submit, prevent default submit behaviour
-  document.querySelector('#compose-form').addEventListener("submit", event => {
-      event.preventDefault();
-      send_mail();
-  });
+    // Send mail on form submit, prevent default submit behaviour
+    document.querySelector('#compose-form').addEventListener("submit", event => {
+        event.preventDefault();
+        send_mail();
+    });
 
 });
 
 function compose_email() {
 
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+    // Show compose view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+    // Clear out composition fields
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
 }
 
 function load_mailbox(mailbox) {
   
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+    // Show the mailbox and hide other views
+    document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+    // Show the mailbox name + create new div to separate h3 from emails
+    document.querySelector('#emails-view').innerHTML = `
+        <h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>
+        <div id="emails-box"></div>
+    `;
+
+    // fetch mailbox data
+    fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+
+        // Loop over emails and create HTML element for each one
+        emails.forEach(email => {
+
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <p>FROM: ${email.sender}</p>
+                <p>TO: ${email.recipients}</p>
+                <p>Subject: ${email.subject}</p>
+                <p>${email.timestamp}</p>
+            `;
+            element.className = 'd-flex justify-content-between list-group-item';
+            element.id = 'test-id'
+            element.addEventListener('click', function() {
+                console.log(email)
+            });
+            document.querySelector('#emails-box').append(element);
+
+        })
+    });
 }
 
 function send_mail() {
